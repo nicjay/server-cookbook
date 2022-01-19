ece4a9ddbdfdf8ec3aa96b5b8d8ec5b37cf3655d
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2022-01-19 08:33:35 -0800

    Consistency pass

--- scripts/compose-update.sh
+++ scripts/compose-update.sh
@@ -1,26 +1,13 @@
 #! /bin/bash
 
-# For each service with a valid compose.yml: pull latest images, update containers, remove dangling images
+# For each service: pull latest and (re)create
+find -name 'docker-compose.yml' -exec docker compose -f {} pull \;
+find -name 'docker-compose.yml' -exec docker compose -f {} up -d --remove-orphans \;
 
-for dir in services/*/
-do
-    #echo "---Pulling ${dir%*/}---"
-    if [ -e ${dir}compose.yml ]
-    then
-        #echo ---Pulling `basename $dir`---
-        docker compose -f ${dir}compose.yml pull
-    fi
-done
+# Remove dangling images
+yes | docker image prune
 
-for dir in services/*/
-do
-    if [ -e ${dir}compose.yml ]
-    then
-        #echo ---Creating `basename $dir`---
-        docker compose -f ${dir}compose.yml up -d --remove-orphans
-    fi
-done
 
-#echo ---Pruning---
-echo "Y" | docker image prune # Answers "Yes" automatically to prompt
+
+
 

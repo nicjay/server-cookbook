561fd29d55d5056bdd390e849532f70f39254236
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2022-01-31 18:23:07 -0800

    Add SWAG dashboard mod; further squish scripts

--- scripts/compose-update.sh
+++ scripts/compose-update.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 # For each service: pull latest and (re)create
-find -name 'docker-compose.yml' -exec docker compose -f {} pull \;
-find -name 'docker-compose.yml' -exec docker compose -f {} up -d --remove-orphans \;
+find -name 'docker-compose.yml' -exec docker compose -f {} pull \; -exec docker compose -f {} up -d --remove-orphans \;
 
 # Remove dangling images
 yes | docker image prune

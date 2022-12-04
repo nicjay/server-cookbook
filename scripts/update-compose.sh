b1652377114a00cd68fbf831c921ff0377a40b4c
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2022-12-04 13:54:31 -0800

    Print version on compose update

--- scripts/update-compose.sh
+++ scripts/update-compose.sh
@@ -3,7 +3,9 @@
 # Install latest Docker Compose release
 # Source: https://docs.docker.com/compose/cli-command/#install-on-linux
 
+CURRENT_VER=$(docker compose version)
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-docker compose version
+echo "Old: $CURRENT_VER"
+echo "New: `docker compose version < $0`"
